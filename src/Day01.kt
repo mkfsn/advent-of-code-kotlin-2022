cbf1f9f6b30b949f8906441128ea0130420fb940
Pei-Ming Wu <p408865@gmail.com>
2022-12-02 01:29:02 +0800

    feat: 2022 Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,29 @@
 fun main() {
+    fun makeGroups(input: List<String>): List<Int> {
+        return input.fold(mutableListOf(mutableListOf<String>())) { acc, ele ->
+            if (ele == "") {
+                acc.add(mutableListOf())
+            } else {
+                acc.last().add(ele)
+            }
+            acc
+        }.map {
+            nums -> nums.sumOf { it.toInt() }
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return makeGroups(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return makeGroups(input).sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
